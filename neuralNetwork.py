91ed3a389885f2ca6fabfadc45f76595473aced3
cream136 <stefankuehl88@gmx.de>
2026-06-11 11:10:18 +0200

    lernrate +

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -3,8 +3,14 @@ import matplotlib.pyplot as plt
 from scipy import special
 
 class neuralNetwork:
-    def __init__(self):
-        pass
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate=1):
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+        self.learning_rate = learning_rate
+        # Hier würden normalerweise die Gewichte initialisiert werden
+        # self.weights_input_hidden = np.random.randn(self.hidden_nodes, self.input_nodes)
+        # self.weights_hidden_output = np.random.randn(self.output_nodes, self.hidden_nodes)
 
     def train(self):
         print("I´am training")
