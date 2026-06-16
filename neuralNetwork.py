834e42da4d96a50f1f707e5c8f42a95316dc649a
cream136 <stefankuehl88@gmx.de>
2026-06-16 14:20:37 +0200

    Backpropagation-Algorithmus implementiert.

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,3 +1,8 @@
+# To execute in console before execute this file: 
+# 1. python3 -m pip install numpy
+# 2. python3 -m pip install scipy
+# 3. python3 -m pip install matplotlib
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import special
@@ -15,10 +20,45 @@ class neuralNetwork:
         # self.who: Gewichte zwischen Hidden- und Output-Layer
         self.who = np.random.normal(0.0, pow(self.hidden_nodes, -0.5), (self.output_nodes, self.hidden_nodes))
 
-    def train(self):
-        print("I´am training")
+    def train(self, inputs_list, targets_list):
+        # Wir trainieren über mehrere Epochen, um die Gewichte zu optimieren
+        for epoch in range(10000):
+            for i in range(len(inputs_list)):
+                # --- Forward Pass ---
+                inputs = np.array(inputs_list[i])
+                target = np.array(targets_list[i])
+
+                # Hidden Layer Aktivierungen
+                hidden_inputs = np.dot(self.wih, inputs)
+                hidden_outputs = special.expit(hidden_inputs)
+
+                # Output Layer Aktivierungen
+                final_inputs = np.dot(self.who, hidden_outputs)
+                final_outputs = special.expit(final_inputs)
+
+                # --- Backward Pass (Backpropagation) ---
+                
+                # 1. Fehler am Output berechnen
+                output_errors = target - final_outputs
+                
+                # Gradient für den Output Layer (Fehler * Ableitung der Sigmoid-Funktion)
+                # Ableitung von Sigmoid(x) ist Sigmoid(x) * (1 - Sigmoid(x))
+                output_gradient = output_errors * final_outputs * (1.0 - final_outputs)
+                
+                # Gewichte who aktualisieren: Delta = learning_rate * gradient * hidden_outputs
+                self.who += np.outer(output_gradient, hidden_outputs) * self.learning_rate
+
+                # 2. Fehler zurück an Hidden Layer leiten
+                hidden_errors = np.dot(self.who.T, output_gradient)
+                
+                # Gradient für den Hidden Layer
+                hidden_gradient = hidden_errors * hidden_outputs * (1.0 - hidden_outputs)
+                
+                # Gewichte wih aktualisieren: Delta = learning_rate * gradient * inputs
+                self.wih += np.outer(hidden_gradient, inputs) * self.learning_rate
 
     def query(self, inputs_list):
+        # ...existing code...
         # Konvertiere die Eingabeliste in ein NumPy-Array
         inputs = np.array(inputs_list)
 
@@ -35,3 +75,23 @@ class neuralNetwork:
         final_outputs = special.expit(final_inputs)
 
         return final_outputs
+
+# create instance of neural network
+n = neuralNetwork(2, 3, 1)
+
+# Trainingsdaten für XOR-Problem
+inputs_list = [[0, 0], [0, 1], [1, 0], [1, 1]]
+targets_list = [[0], [1], [1], [0]]
+
+# train the neural network
+print("Training started... please wait.")
+n.train(inputs_list, targets_list)
+print("Training finished!")
+
+# query the network to test it
+print("\nTesting the network:")
+for i in range(len(inputs_list)):
+    res = n.query(inputs_list[i])
+    print(f"Input: {inputs_list[i]} -> Target: {targets_list[i]} -> Prediction: {res}")
+    
+print("\nperformance = great")
